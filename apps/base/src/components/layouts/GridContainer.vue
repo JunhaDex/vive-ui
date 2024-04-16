8267ae7f74fa318091b1733cc159ccbf718e9ec3
JunhaDex <dk.junha.dev@gmail.com>
2024-04-16 18:59:15 +0900

    add container

---
+++ apps/base/src/components/layouts/GridContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid rw h-full" :class="`grid-cols-${col} grid-rows-${row}`">
+    <slot />
+  </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+  col: number
+  row: number
+}>()
+</script>
+<style scoped>
+.rw {
+  margin: 0 6%;
+  @media (min-width: 768px) {
+    margin: 0 auto;
+    max-width: 688px;
+  }
+  @media (min-width: 1024px) {
+    margin: 0 auto;
+    max-width: 928px;
+  }
+  @media (min-width: 1312px) {
+    margin: 0 auto;
+    max-width: 1152px;
+  }
+}
+</style>
--- apps/base/src/views/Guide.vue
+++ apps/base/src/views/Guide.vue
@@ -2,6 +2,9 @@
   <section>
     <h1 class="text-3xl">foo</h1>
     <button class="btn btn-primary">Button</button>
+    <GridContainer col="1" row="1">foo</GridContainer>
   </section>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import GridContainer from '@/components/layouts/GridContainer.vue'
+</script>
